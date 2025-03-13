69a1650950064fb16d4a2b19482b2a4957252f3d
Kian Bador <106530735+kianbador@users.noreply.github.com>
2025-03-13 10:21:47 +0800

    dasd

---
+++ system-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        $user = User::all();  
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $user
+        ]);
+    }
+}
